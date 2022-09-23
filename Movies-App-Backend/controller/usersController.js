847d77ff6f9ff49abcc9d5f49ce5fa9b3205af4c
juanIgnacio <juan.melgarejobrabo@gmail.com>
2022-09-23 00:26:51 -0300

    code perfomance

--- Movies-App-Backend/controller/usersController.js
+++ Movies-App-Backend/controller/usersController.js
@@ -1,6 +1,6 @@
 const userModel = require("../model/usersModel");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 module.exports = {
   create: async function (req, res, next) {
     try {
@@ -9,37 +9,36 @@ module.exports = {
         password: req.body.password,
       });
       const docu2 = await docu.save();
-      res.status(200).json({docu2:docu2,message:"you sign-up correctly"});
+      res.status(200).json({ docu2: docu2, message: "you sign-up correctly" });
     } catch (e) {
-
-      if(e.code = 11000){
-        res.status(400).json("The Email already exist")
-       
-        //
+      if ((e.code = 11000)) {
+        res.status(400).json({message:"The Email already exist"});
       }
-      
+
       next(e);
     }
   },
 
-  getAll: async function(req,res,next){
-      const user = await userModel.find()
-      res.status(200).json(user)
+  getAll: async function (req, res, next) {
+    const user = await userModel.find();
+    res.status(200).json(user);
   },
 
   login: async function (req, res, next) {
     try {
       const user = await userModel.findOne({ email: req.body.email });
-     
+
       if (!user) {
-        res.json({message: "usuario y/o contraseña incorrectos"});
+        res.json({ message: "incorrect email or password" });
         return;
       }
       if (bcrypt.compareSync(req.body.password, user.password)) {
-        const token = jwt.sign({userId: user._id},"movies",{expiresIn:"1h"})
-        res.json(token);
+        const token = jwt.sign({ userId: user._id }, "movies", {
+          expiresIn: "1h",
+        });
+        res.status(200).json({token,message:"Autorized"});
       } else {
-        res.json({message: "usuario y/o contraseña incorrectos"});
+        res.json({ message: "incorrect email or password" });
         return;
       }
     } catch (e) {
